d0a29ad8b6f568d9b156409cde32c17b00081274
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-22 17:00:48 +0330

    sadeghi

---
+++ another.py
@@ -0,0 +1,80 @@
+# print(1+2)
+# print("1+2")
+# print("Hello world")
+# print(Hello world)  # error
+# print('hello world')
+# print("Your new score is", 1000 + 1)
+# print("Your new score is", "1000 + 1")
+
+# توضیحات
+# print("he said \"i want to go school\"")
+# print()
+# print("hello every body. \nwelcome to our python class. \nwe are going to learn every things about python from zero to hero!")
+# print()
+# print("""hello every body.
+# welcome to our python class.
+# we are going to learn every things about python from zero to hero!""")
+
+# print(1+5)
+# x = 4 * 7
+# print(x)
+
+
+# score = 0
+# print(type(score))
+# print("score:", score)
+# score = score + 1
+# print("new score:", score)
+# score += 1
+# print("new score:", score)
+# print()
+# score = score - 1
+# print("new score:", score)
+# score -= 1
+# print("new score:", score)
+
+# نامگذاری متغیرها در پایتون
+# هرترکیبی از حروف الفبای انگلیسی و عدد و آندرلاین می تونه بعنوان نام متغیر استفاده بشه به جز:
+# اینکه اولین کاراکتر نباید عدد باشه
+
+# score1 = 12
+# s1 = 13
+# s_1 = 15
+# 1s = 12# error
+# score 1 = 12  # error
+
+# عملیات ریاضی در پایتون
+# a = 3
+# b = 2
+
+# c = a + b
+# print("c =", c)
+
+# d = a - b
+# print("d =", d)
+
+# e = a * b
+# print("e =", e)
+
+# f = a / b
+# print("f =", f)
+
+# g = a // b
+# print("g =", g)
+
+
+# باقیمانده
+# number = 4
+# print(number % 2)
+
+# number = 5
+# print(number % 2)
+
+
+# x = 2 + 3 * 2
+# print("x =", x)
+# y = (2 + 3) * 2
+# print("y =", y)
+
+x = input('enter a number: ')
+print(type(x))
