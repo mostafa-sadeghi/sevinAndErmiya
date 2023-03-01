c1e5ca4e55867303ac2e1fa785e7878657a87d26
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-01 16:58:34 +0330

    sadeghi

---
+++ s4.py
@@ -0,0 +1,33 @@
+# name = input("what's your name? ")
+# family = input("what's your family? ")
+# print('hello ' + name + ' ' + family)
+
+
+# number1 = float(input('enter a number: '))
+# number2 = float(input('enter a number: '))
+# print(number1 + number2)
+
+# int   عدد صحیح
+# int('1')
+# float  عدد اعشاری
+# float('1.5')
+
+# اسم یک دانش آموز را از ورودی بگیرد
+# نمره سه درس را از ورودی بگیرد
+# معدل دانش آموزا را محاسبه کنید
+# معدل دانش آموزا را به همراه اسمش پرینت کنید
+# معدل = نمره درس اول + نمره در دوم + نمره درس سوم و درنهایت تقسیم بر تعدادشان
+# ali's average is : 19
+
+
+name = "tina"
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+# print(name[4])  # error
+print("tina has", len(name), "characters")
+
+# exercise 2: برنامه ای بنویسید که نا شما را از ورودی دریافت نماید و تمام کاراکترهای
+# نامتان را پرینت نمائید
+# در انتها طول نامتان را پرینت نمائید.
