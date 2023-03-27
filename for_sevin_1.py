ad89bb37557b2145423c3c624561962ce21b9334
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-27 16:58:54 +0330

    sadeghi

---
+++ for_sevin_1.py
@@ -0,0 +1,52 @@
+# message = "hello every body.\nwelcome to our python class.\nwe are going to learn every things about python.\npython from zero to hero."
+# print(message)
+# message = """hello every body.
+# welcome to our python class.
+# we are going to learn every things about python.
+# python from zero to hero."""
+# print(message)
+
+
+# my_str = "He said, \"Aren't can't shouldn't wouldn't.\""
+# print(my_str)
+
+
+# my_str = 'He said, "Aren\'t can\'t shouldn\'t wouldn\'t."'
+# print(my_str)
+
+# my_str = '''He said, "Aren't can't shouldn't wouldn't."'''
+# print(my_str)
+
+
+# name = input('enter your name please: ')
+# age = int(input('enter your age please:> '))
+
+# print(name + " has " + str(age) + " years old ")
+
+# برنامه ای بنویس که نام، نام خانوادگی و ورزش مورد علاقه دانش آموز را از ورودی بگیرد و
+# پیغامب مشابه زیر پرینت نماید:
+# sara sabouri likes tennis.
+
+
+myscore = 1000
+largest_score = 10000
+# message = "my score is " + str(myscore)
+# message = "my score is %s"%myscore
+# print(message)
+message = "my score is %s and largest score is %s" % (myscore, largest_score)
+print(message)
+message = f"my score is {myscore} and largest score is {largest_score}"
+print(message)
+
+#  برنامه ای بنویس که نام سه دوست را از ورودی بگیرد و با روش های گفته شده
+# یعنی با کمک
+#  +
+
+# و با کمک
+# %s
+
+# وبا کمک
+# f
+# پیغام زیر را پرینت نماید
+
+# sara and tina and roze are friends.
