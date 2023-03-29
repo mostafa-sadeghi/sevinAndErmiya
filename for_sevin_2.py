e45644b94083650998daba19d86611eec5d328bf
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-29 16:58:12 +0330

    sadeghi

---
+++ for_sevin_2.py
@@ -0,0 +1,42 @@
+# print('*' * 10)
+
+# name = input('enter a name:> ')
+# number = int(input('How many times do you want to repeat? '))
+
+# print((name + ' ') * number)
+
+# spaces = ' ' * 25
+# print('%s welcome' % spaces)
+# print('%s Python class' % spaces)
+# print('%s pygame class' % spaces)
+# print()
+# print()
+# print('Dear Sir')
+# print()
+# print('I wish to report that tiles are missing from the')
+# print('outside toilet roof.')
+# print('I think it was bad wind the other night that blew them away.')
+# print()
+# print('Regards')
+# print('Malcolm Dithering')
+
+
+# shopping_list = 'egg apple pen mashroom pencil'
+# یک ایتم به فهرست بالا اضافه کنی
+# tomato  potato
+# می تونی تمام ایتم های لیست خریدت رو جداگانه پرینت کنی و نمایش بدی؟
+# shopping_list = shopping_list +" "+ "tomato"
+# print(shopping_list)
+# shopping_list = shopping_list + " " + "potato"
+# print(shopping_list)
+
+# print()
+# print(shopping_list[0:3])
+# print(shopping_list[4:9])
+
+shopping_list = []
+print(type(shopping_list))
+shopping_list.append('egg')
+shopping_list.append('apple')
+shopping_list.append('mashroom')
+print("shopping list items are:", shopping_list)
