b85f4f447c0cafed55b49ae9a0ab0aaa91eca02d
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-01 16:58:40 +0330

    sadeghi

--- for_sevin_2.py
+++ for_sevin_2.py
@@ -34,9 +34,46 @@
 # print(shopping_list[0:3])
 # print(shopping_list[4:9])
 
-shopping_list = []
-print(type(shopping_list))
-shopping_list.append('egg')
-shopping_list.append('apple')
-shopping_list.append('mashroom')
-print("shopping list items are:", shopping_list)
+# shopping_list = []
+# print(type(shopping_list))
+# shopping_list.append('egg')
+# shopping_list.append('apple')
+# shopping_list.append('mashroom')
+# print("shopping list items are:", shopping_list)
+
+# برنامه ای بنویس که سه عدد را از  ورودی دریافت کند و اعداد را به یک رشته اضافه نماید
+# در انتها مجموع اعداد موجود در رشته بالا را محاسبه و نمایس دهد
+
+# n1 = int(input("enter a number:> "))
+# n2 = int(input("enter a number:> "))
+# n3 = int(input("enter a number:> "))
+# string = ''
+# string += str(n1)+ ',' + str(n2) + ',' + str(n3)
+# print(string)
+# numbers = string.split(',')
+# print(numbers)
+# print(int(numbers[0]) + int(numbers[1]) + int(numbers[2]))
+# n1 = string[:2]
+# n2 = string[3:6]
+# n2 = string[6:]
+# print(n1)
+# print(n2)
+# print(n3)
+# print(int(n1) + int(n2) + int(n3))
+# numbers = input('enter three numbers with , seperator:> ')
+# numbers = numbers.split(',')
+# new_numbers = [int(numbers[0]), int(numbers[1]), int(numbers[2])]
+# print(new_numbers)
+# print(sum(new_numbers))
+# print("sum of all numbers :", new_numbers[0] + new_numbers[1] + new_numbers[2])
+
+
+# کارهای بالا را به کمک لیست انجام بده.
+
+
+# x = str(123.45)
+# print(x)
+# print(type(x))
+# y = str(True)
+# print(y)
+# print(type(y))
