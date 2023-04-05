b7dbaf760232a8d8baa76a51440ad71d0ec9dc2c
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-05 16:55:15 +0330

    sadeghi

---
+++ s5.py
@@ -0,0 +1,76 @@
+# a = 40
+# b = 5
+# if a < b:
+#     print("a is less than b")
+#     print()
+
+# print("outside")
+
+
+# a = 4
+# b = 4
+
+# if a <= b:
+#     print("a is less than or equal to b")
+# if a >= b:
+#     print("a is greater than or equal to b.")
+
+
+# if a < b or a == b:
+#     print("a is less than or equal to b")
+# if a > b or a == b:
+#     print("a is greater than or equal to b.")
+
+
+# x = 3
+# y = 4
+
+# if x == y:
+#     print("x and y are equal...")
+
+# if x != y:
+#     print("x and y are not equal...")
+
+
+# تمرین برنامه ای بنویسید که سه عدد از ورودی بگیرد
+# بزرگترین عدد را نمایش دهد
+# کوچکترین عدد را نمایش دهد
+# میانگین اعداد را محاسبه نماید
+# میانگین یعنی جمع عددعا تقسیم بر تعدادشون
+
+
+# x = 12
+# y = 14
+# z = 10
+
+# if x < y and x < z:
+#     print("x is lower than y and z")
+
+# if x < y or x < z:
+#     print("x is lower than y or z")
+
+
+# a = False
+# if a:
+#     print("a is true")
+
+# if not a:
+#     print("a is false")
+
+a = True
+b = False
+
+if a and b:
+    print("one")
+
+if a or b:
+    print("three")
+
+
+a = True
+b = True
+if a and b:
+    print("two")
+
+if a or b:
+    print("another")
