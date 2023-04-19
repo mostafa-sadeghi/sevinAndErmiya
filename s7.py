009d9ffeda5571bda3472bd2830d3bafa4aa7a1d
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-19 17:03:24 +0330

    sadeghi

---
+++ s7.py
@@ -0,0 +1,30 @@
+# for s in 'python':
+#     print(s, end=' ')
+
+# print()
+
+# total = 0
+# for number in [2,4,6,8,9,0,-1]:
+#     total += number # total = total + number
+
+# print(total)
+
+
+
+# for i in range(3):
+#     print("a")
+#     print(i)
+
+
+# for i in range(3):
+#     print("a")
+
+# for j in range(3):
+#     print("b")
+
+
+# for i in range(3):
+#     print("a")
+
+#     for j in range(3):
+#         print("b")
