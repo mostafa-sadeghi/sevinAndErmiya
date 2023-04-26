104addc4e78115ac26393d52cda97ad946d77801
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-26 16:58:03 +0330

    sadeghi

--- s8.py
+++ s8.py
@@ -16,3 +16,15 @@
 # exercise برنامه ای بنویسید که مجموع اعداد بین پنج تا صدوبیست و پنج را محاسبه و نمایش دهد
 
 
+zeros = 0
+i = 0
+
+while i < 5:
+    new_number = int(input('enter a number : '))
+    if new_number == 0:
+        zeros += 1
+    i += 1
+
+print("number of zeros:", zeros)
+
+# exercise 2 : برنامه بالا را با کمک حلقه فور بنویسید
