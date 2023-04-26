8a95af078238119b39f1e4e90b04b355bc72f494
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-26 16:44:56 +0330

    sadeghi

---
+++ s8.py
@@ -0,0 +1,18 @@
+# i = 0
+# total = 0
+# while i < 5:
+#     new_number = int(input('enter a number: '))
+#     total += new_number
+#     i = i + 1
+# print("total is :", total)
+
+
+# for i in range(5):
+#     new_number = int(input('enter a number: '))
+#     total += new_number
+
+# print("total is :", total)
+
+# exercise برنامه ای بنویسید که مجموع اعداد بین پنج تا صدوبیست و پنج را محاسبه و نمایش دهد
+
+
